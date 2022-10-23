e0a5f2571ba67742756a1945de9a51eef9812764
Фанит Загитов <fanit666@gmail.com>
2022-10-23 17:08:03 +0700

    test rtdb on my pc

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Monitel.Rtdb.Api;
 
 namespace ConsoleApp
 {
@@ -10,6 +11,20 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (Monitel.Rtdb.Api.IRtdbProvider provider = Monitel.Rtdb.Api.RtdbProvider.CreateProvider())
+            {
+                Monitel.Rtdb.Api.IRtdbProxy proxy;
+                var connectionString = "CK11-IA-3.pl03n/lcl";
+                try
+                {
+                    proxy = provider.Connect(connectionString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
         }
     }
 }
